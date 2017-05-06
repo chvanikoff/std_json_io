17f44d56f8ba42b8aeaa3b5ecebdfebf9aeffa72
Roman Chvanikov <chvanikoff@gmail.com>
2017-05-06 22:29:21 +0900

    Added test for StdJsonIo.json_call and StdJsonIo.json_call!

--- test/std_json_io_test.exs
+++ test/std_json_io_test.exs
@@ -6,4 +6,16 @@ defmodule StdJsonIoTest do
     {:ok, _} = StdJsonIoMock.start_link([])
     {:ok, %{}}
   end
+
+  test "Call to json_call returns correct value" do
+    message = %{"hello" => "world"}
+    expected = {:ok, %{"response" => message}}
+    assert StdJsonIoMock.json_call(message) == expected
+  end
+
+  test "Call to json_call! returns correct value" do
+    message = %{"hello" => "world"}
+    expected = %{"response" => message}
+    assert StdJsonIoMock.json_call!(message) == expected
+  end
 end
